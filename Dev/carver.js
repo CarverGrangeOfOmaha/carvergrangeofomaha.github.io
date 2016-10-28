6b4c9bb7d680c850773dd75a7005a4e928754e32
distraught1 <nickweddleton@gmail.com>
2016-10-27 20:20:04 -0500

    Responsive changes

---
+++ Dev/carver.js
@@ -0,0 +1,9 @@
+$("#mobileNav").click(function(){
+    $("nav").show();
+    $("#mobileNavScreen").show();
+});
+
+$("#mobileNavScreen").click(function(){
+    $("nav").hide();
+    $("#mobileNavScreen").hide();
+});
