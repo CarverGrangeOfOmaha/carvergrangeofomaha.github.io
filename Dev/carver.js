862deb328a49ab0f2940d682fd808c8c9e0be3f0
distraught1 <nickweddleton@gmail.com>
2016-11-03 11:41:08 -0500

    Final iOS Fixes

--- Dev/carver.js
+++ Dev/carver.js
@@ -1,19 +1,9 @@
-$(document).bind("mobileinit", function () {
-    // jQuery Mobile's Ajax navigation does not work in all cases (e.g.,
-    // when navigating from a mobile to a non-mobile page), especially when going back, hence disabling it.
-    $.extend($.mobile, {
-        ajaxEnabled: false
-    });
+$("#mobileNav").on("click touch", function(){
+    $("nav").show();
+    $("#mobileNavScreen").show();
 });
 
-$(document).ready(function () {
-    $("#mobileNav").on("click touch", function(){
-        $("nav").css("display", "block");
-        $("#mobileNavScreen").show();
-    });
-
-    $("#mobileNavScreen").on("click touch", function(){
-        $("nav").css("display", "none");
-        $("#mobileNavScreen").hide();
-    });
+$("#mobileNavScreen").on("click touch", function(){
+    $("nav").hide();
+    $("#mobileNavScreen").hide();
 });
